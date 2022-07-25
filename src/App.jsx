89d1fe8042e176cb445a80d38e5b6aff547f19c2
Tamicktom <rickpokemon38@gmail.com>
2022-07-25 16:51:39 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import React, { useState, useId } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useId, useEffect } from 'react';
 import './App.css';
 import SBR from './caixas/sbr/SBR.jsx';
+import SuperValle from './caixas/super_valle/SuperValle';
 
 const optionsMercado = [
-  "sbr",
-  "pessoto",
-  "ponto_certo", "ja",
-  "falavina",
-  "supervalle_1",
-  "supervalle_2"
+  "SBR",
+  "SuperValle",
 ];
+
+const tipos = {
+  SBR: <SBR />,
+  SuperValle: <SuperValle />,
+}
 
 function App() {
   const [preco, setPreco] = useState(0);
   const [descricao, setDescricao] = useState(0);
-  const [mercado, setMercado] = useState(0);
+  const [mercado, setMercado] = useState("SBR");
   const id = useId();
+
+  const priceTag = (tipo, props) => {
+    if (mercado == "SBR") {
+      return (
+        <SBR
+          tipo={tipo}
+          key={id + mercado + tipo + Math.random()}
+          {...props}
+        />)
+    }
+    if (mercado == "SuperValle") {
+      return (<SuperValle
+        tipo={tipo}
+        key={id + mercado + tipo + Math.random()}
+        {...props}
+      />)
+    }
+  }
 
   function tratarPrecos(precos) {
     let array = precos.split("\n");
@@ -26,6 +45,21 @@ function App() {
   function tratarDescricao(des) {
     des = des.split("\n");
     return des;
+  }
+
+  const selectOptions = (optionsList) => {
+    let options = [];
+    const id = useId();
+    for (let i = 0; i < optionsList.length; i++) {
+      options.push(
+        <option
+          key={id + i}
+          value={optionsList[i]}
+        >
+          {optionsList[i]}
+        </option>);
+    }
+    return options;
   }
 
   function boxers() {
@@ -45,15 +79,16 @@ function App() {
 
       try {
         if (tmp[1].length < 2) tmp[1] += "0";
+        const props = {
+          preco_grande: tmp[0],
+          preco_pequeno: tmp[1],
+          descricao_cima: desc[0].toString().replace(/\,/g, " ").toUpperCase(),
+          descricao_baixo: desc[1].toString().replace(/\,/g, " ").toUpperCase(),
+          tipo: tipo,
+        }
         tag.push(
-          <SBR
-            key={id + i}
-            tipo={tipo}
-            preco_grande={tmp[0]}
-            preco_pequeno={tmp[1]}
-            descricao_cima={(desc[0].toString()).replace(/\,/g, " ").toUpperCase()}
-            descricao_baixo={(desc[1].toString()).replace(/\,/g, " ").toUpperCase()}
-          />)
+          priceTag(tipo, props)
+        )
       } catch (e) {
 
       }
@@ -67,9 +102,12 @@ function App() {
         <div className="left">
           <h1>Gerador de caixas</h1>
           <select
-            onChange={(e) => { setMercado(e.target.value) }}
             name="Mercado"
-            id="mercado">
+            id="mercado"
+            onBlur={(e) => {
+              setMercado(e.target.value)
+            }}
+          >
             {selectOptions(optionsMercado)}
           </select>
         </div>
@@ -102,19 +140,7 @@ function App() {
   );
 }
 
-const selectOptions = (optionsList) => {
-  let options = [];
-  const id = useId();
-  for (let i = 0; i < optionsList.length; i++) {
-    options.push(
-      <option
-        key={id + i}
-        value={optionsList[i]}>
-        {optionsList[i]}
-      </option>);
-  }
-  return options;
-}
+
 
 
 export default App;
